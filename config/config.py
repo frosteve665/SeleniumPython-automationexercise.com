13192897f890d94f6d446017b646aa74a9fbb90f
frosteve6653656 <mude66@mhs.itenas.ac.id>
2024-12-03 21:22:05 +0700

    Testcase 005 - Register User with existing email

--- config/config.py
+++ config/config.py
@@ -4,6 +4,5 @@ class TestData:
     IMPLICIT_WAIT = 10
     EXPLICIT_WAIT = 10
     TEST_USER_NAME = "Lucius Norris"
-    TEST_USER_NAME = "Lucius Norris"
     TEST_USER_EMAIL = "alfiansyah@gmail.com"  # Keeping your email
     TEST_USER_PASSWORD = "whoareyou"
--- pages/login_page.py
+++ pages/login_page.py
@@ -4,12 +4,24 @@ from selenium.webdriver.common.by import By
 
 class LoginPage(BasePage):
     # Locators
+    # Login form elements
     LOGIN_EMAIL = (By.CSS_SELECTOR, "input[data-qa='login-email']")
     LOGIN_PASSWORD = (By.CSS_SELECTOR, "input[data-qa='login-password']")
     LOGIN_BUTTON = (By.CSS_SELECTOR, "button[data-qa='login-button']")
     LOGIN_FORM = (By.CSS_SELECTOR, ".login-form")
     LOGIN_HEADING = (By.CSS_SELECTOR, ".login-form h2")
     LOGIN_ERROR = (By.CSS_SELECTOR, "p[style='color: red;']")
+
+    # Signup form elements
+    SIGNUP_HEADING = (By.XPATH, "//div[@class='signup-form']/h2")  # More specific selector
+    SIGNUP_NAME = (By.NAME, "name")
+    SIGNUP_EMAIL = (By.CSS_SELECTOR, "input[data-qa='signup-email']")
+    SIGNUP_PASSWORD = (By.CSS_SELECTOR, "input[data-qa='signup-password']")
+    SIGNUP_BUTTON = (By.CSS_SELECTOR, "button[data-qa='signup-button']")
+
+    def verify_signup_heading(self):
+        heading = self.get_text(*self.LOGIN_HEADING)
+        return "New User Signup!" in heading
 
     def is_login_form_visible(self):
         """Verify if login form is visible"""
@@ -23,10 +35,16 @@ class LoginPage(BasePage):
     def login(self, email, password):
         """Login with email and password"""
         self.type_text(*self.LOGIN_EMAIL, email)
-        self.type_text(*self.LOGIN_PASSWORD, password)
+        self.type_text(*self.LOGIN_PASSWORD, password) 
         self.click(*self.LOGIN_BUTTON)
 
-    def verify_error_message(self):
-        """Verify error message for invalid credentials"""
-        error = self.get_text(*self.ERROR_MESSAGE)
-        return "Your email or password is incorrect!" in error
+    def verify_signup_heading(self):
+        heading = self.get_text(*self.SINGUP_HEADING)
+        return "New User Signup!" in heading
+
+    def signup(self, name, password):
+        """Signup with email and password"""
+        self.type_text(*self.SIGNUP_NAME, name)
+        self.type_text(*self.SIGNUP_EMAIL, password)
+        self.click(*self.SIGNUP_BUTTON)
+
